f541800331e286812c034fd22e3a5d85b7c96601
Vaughan Hilts <hilts.vaughan@gmail.com>
2016-03-14 11:24:23 -0400

    server - core: service stubs

---
+++ server/services/ProductLookupService.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * Provides facilities for looking up a product
+ */
+class ProductService {
+  constructor(lookupStrategy) {
+    this.strategy = strategy
+  }
+
+  getProductByImageUrl(imageUrl, callback) {
+    //TODO: Get the image by the product URL
+  }
+
+  getProductPrice(product) {
+    // TODO: Get the product pricing information
+  }
+
+  getProductDetails() {
+      // TODO: Get the product details
+  }
+
+}
+
+module.exports = ProductService
