22a96c55bae80464b5de2997d464ee4de115c9f5
KairuRengu <hyle200@gmail.com>
2016-03-16 12:13:35 -0400

    Changed the unit test ImageHasherTest.js
    Edited the ImageHasherTest.js so that it would only work in the server
    or tests directory. All other directories it'll just toss a console
    error

--- server/tests/ImageHasherTest.js
+++ server/tests/ImageHasherTest.js
@@ -2,20 +2,31 @@ var assert = require('chai').assert
 var ImageHasher = require('../lib/ImageHasher')
 
 describe('ImageHasher', function() {
-  describe('#getImageBase64String', (done) => {
+	var commandDirectory = process.cwd()
+	console.log(commandDirectory)
+	var commandDirectoryIndex = commandDirectory.lastIndexOf('\\') + 1
+	commandDirectory = commandDirectory.substring(commandDirectoryIndex, commandDirectory.length)
+	describe('#getImageBase64String', (done) => {
     var hasher = new ImageHasher()
-    it('should return a proper encoding for a file that exists', (done) => {
-      hasher.getImageBase64String("res/homura.jpg", (hash) => {
-        assert(hash === '0abed64929fcb3e713b4830b9e18387af818a7062921f3aaeb5a38a1110fe162')
-        done()
-      })
-    })
+	if (commandDirectory == "server"){
+		it('should return null if the file is missing', (done) => {
+			hasher.getImageBase64String('tests/res/homura', (hash) => {
+				assert.isNull(hash)
+				done()
+			})
+		})
+	}else if(commandDirectory == "tests"){
+		it('should return null if the file is missing', (done) => {
+			hasher.getImageBase64String('res/homura', (hash) => {
+				assert.isNull(hash)
+				done()
+			})
+		})
+	}else{
+		console.log("Please run the command in the server or tests directory")
+	}
 
-    it('should return null if the file is missing', (done) => {
-      hasher.getImageBase64String('res/homura', (hash) => {
-        assert.isNull(hash)
-        done()
-      })
-    })
+
+
   })
 })
